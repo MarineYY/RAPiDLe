72b0469c8b1e52d767cfd59eb73ceb64401ca7e4
MARINE <22351213@zju.edu.cn>
2024-10-27 11:05:03 +0800

    Update kafka.py

--- log_producer/trace/kafka.py
+++ log_producer/trace/kafka.py
@@ -1,7 +1,7 @@
 import sys
 from confluent_kafka import Producer
 import json
-sys.path.append('/home/yangyangwei/RAPiDLe/log_producer/trace')
+sys.path.append('path/RAPiDLe/log_producer/trace')
 from config import event_csv_predict_list, log_list, event_dict
 
 json_count = 0

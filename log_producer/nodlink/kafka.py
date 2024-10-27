01194622f2404474e8b666d355c347d982ee3318
MARINE <22351213@zju.edu.cn>
2024-10-27 10:56:20 +0800

    Update kafka.py

--- log_producer/nodlink/kafka.py
+++ log_producer/nodlink/kafka.py
@@ -2,7 +2,7 @@ import sys
 from confluent_kafka import Producer
 import json
 import uuid
-sys.path.append('/home/yangyangwei/RAPiDLe/log_producer/nodlink')
+sys.path.append('path/RAPiDLe/log_producer/nodlink')
 from config import event_csv_predict_list, log_list, event_dict
 
 json_count = 0
